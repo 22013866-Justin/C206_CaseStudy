92d5a97f2403b779efc61d6a8c414c669df13bb8
22013866-Justin <22013866@myrp.edu.sg>
2023-08-03 09:56:52 +0800

    test

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,53 @@
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
-public class C206_CaseStudyTest { //jfjf
+import java.util.ArrayList;
+import java.util.Currency;
+
+public class C206_CaseStudyTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("before C206_CaseStudyTest class");
+	}
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		System.out.println("before");
 	}
 
 	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+	public void enterFeedback() {
+//		ArrayList<Feedback> feedbackList = new ArrayList<Feedback>();
+
+	}
+
+	@Test
+	public void viewFeedback() {
+//		assertEquals(,C206_CaseStudy.ViewFeedback());
+//		assertEquals(null,C206_CaseStudy.ViewFeedback(-12,-1,-3,-4,-2})
+	}
+	public void EnterFeedback() {
+		ArrayList<Feedback> FeedbackArrayList = new ArrayList<Feedback>();
+		System.out.println("start test case addCurrency");
+		System.out.println("test case addCurrency with assertEquals");
+		assertEquals(1,C206_CaseStudy.EnterFeedback(FeedbackArrayList,"123"));
+		assertNotEquals(0,C206_CaseStudy.EnterFeedback(FeedbackArrayList,"okok"));
+	}
+
+	
+
+
+	
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("afters");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("after C206_CaseStudyTest class");
 	}
 
 }
